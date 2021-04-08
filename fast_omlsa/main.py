7091fb18c3c0f3a71d6bb6924de06b5c51fad80b
yuzhou He <yuzhouhe2000@gmail.com>
2021-04-08 13:02:57 +0800

    update

--- fast_omlsa/main.py
+++ fast_omlsa/main.py
@@ -3,23 +3,22 @@ from omlsa import omlsa
 from scipy.io.wavfile import write
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.signal
 
 input1 = "p287_004.wav"
 input2 = "p287_005.wav"
 input_dst = "input.wav"
 out_dst = "out.wav"
 
-
 fs, y1 = read(input1)
-
 y1 = (y1 / 32767).astype(np.float)
+# fs,y2 = read(input2)
+# y2 = (y2 / 32767).astype(np.float)
 
-fs,y2 = read(input2)
-y2 = (y2 / 32767).astype(np.float)
-white_noise = np.random.normal(0,0.02,len(y1))
-
-# y_combine = y1 + y2[0:len(y1)]*0.1
+# # resample if you want
+# y1 = scipy.signal.resample(y1, int(len(y1)/16000*44100))
+white_noise = np.random.normal(0,0.0001,len(y1))
+# y_combine = y1 + y2[0:len(y1)]*0.6
 y_combine = y1 + white_noise
 
 # plt.plot(y_combine)
@@ -28,7 +27,7 @@ y_combine = y1 + white_noise
 write(input_dst,fs,y_combine) 
 
 # choose between f (frequency domain plot), t (time domain plot), or None
-output = omlsa(y_combine,fs, plot = "f")
+output = omlsa(y1,fs, frame_length = 256, frame_move = 128, plot = "f",preprocess = None,high_cut = 8000)
 
 write(out_dst,fs,output) 
 print("done")
--- fast_omlsa/omlsa.py
+++ fast_omlsa/omlsa.py
@@ -6,13 +6,9 @@ import scipy
 import matplotlib.pyplot as plt
 import time
 import scipy.special
-
+from numba import njit, prange
+from scipy.signal import butter, lfilter
 # Some helper functions
-
-# Turn nparray from size (x,) to (x,1)
-def reformat(input):
-    input = input.reshape(len(input),1)
-    return input
 
 # Return real part of exponential integral, same as matlab expint()
 def expint(v):
@@ -22,19 +18,84 @@ def expint(v):
 def circular_shift(x,t):
     return [x[t:len(x)], x[0:t]]
 
+@njit(fastmath=True, cache=True)
+def fast_loop1(N_eff,conv_Y,conv_I,Sft):
+    for i in range(0,N_eff):
+        if int(conv_I[i]) != 0:
+            Sft[i] = np.divide(conv_Y[i],conv_I[i])
+    return Sft
+
+@njit(fastmath=True, cache=True)
+def fast_loop2(N_eff,gamma_mint,gamma1,zeta0,zetat,qhat):
+    for i in range(0,N_eff):
+        if (gamma_mint[i]>1 and gamma_mint[i]<gamma1 and zetat[i]<zeta0):
+            qhat[i] = (gamma1-gamma_mint[i]) / (gamma1-1)
+        elif gamma_mint[i] >= gamma1 or zetat[i] >= zeta0:
+            qhat[i] = 0
+    return qhat
+
+@njit(fastmath=True, cache=True)
+def fast_loop3(N_eff,eta,eta_min):
+    for i in range(0,N_eff):
+        if eta[i] < eta_min:
+            eta[i] = eta_min
+    return eta
+
+@njit(fastmath=True, cache=True)
+def fast_loop4(N_eff,gamma_mint,gamma1,zetat,zeta0,phat):
+    for i in range(0,N_eff):  
+        if (gamma_mint[i] >=gamma1 or zetat[i] >=zeta0):
+            phat[i] = 1
+    return phat
+
+
+njit(fastmath=True, cache=True)
+def fast_loop5(N_eff,I_f,gamma0,zeta,zeta0,gamma_min):
+    for i in range(0,N_eff):
+        if(gamma_min[i] <gamma0 and zeta[i] < zeta0):
+            I_f[i] = 1
+        else:
+            I_f[i] = 0
+    return I_f
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
 # OMLSA + IMCRA algorithm
-def omlsa(input,fs,plot = None):
+def omlsa(raw_input,fs,frame_length,frame_move,plot = None,preprocess = None,high_cut = None):
     start = time.time()
+
+    if (preprocess == "butter"):
+
+        input = butter_bandpass_filter(raw_input,20,high_cut,fs)
+        input = input / max(abs(input))
+
+    elif (preprocess == "ellip"):
+        sos = scipy.signal.ellip(4,5,40,[20/(fs/2),high_cut/(fs/2)],btype='bandpass', output='sos')
+        input = scipy.signal.sosfilt(sos, raw_input)
+        input = input / max(abs(input))
+    else:
+        input = raw_input
     data_length = len(input)
-    frame_length = 512
-    frame_move = 128
     frame_overlap = frame_length - frame_move
     N_eff = int(frame_length / 2 + 1)
     loop_i = 0
-    frame_in = np.zeros((frame_length, 1))
-    frame_out = np.zeros((frame_length, 1))
-    frame_result = np.zeros((frame_length, 1))
-    y_out_time = np.zeros((data_length, 1))
+    frame_in = np.zeros((frame_length, ))
+    frame_out = np.zeros((frame_length, ))
+    frame_result = np.zeros((frame_length, ))
+    y_out_time = np.zeros((data_length, ))
     win = np.hamming(frame_length)
     '''normalization of window'''
     win2 = np.power(win,2)
@@ -66,8 +127,8 @@ def omlsa(input,fs,plot = None):
     beta = 2
     eta_min = 0.0158
     GH0 = np.power(eta_min,0.5)
-    gama0 = 4.6
-    gama1 = 3
+    gamma0 = 4.6
+    gamma1 = 3
     zeta0 = 1.67
     Bmin = 1.66
     l_mod_lswitch = 0
@@ -84,18 +145,17 @@ def omlsa(input,fs,plot = None):
             '''move the frame by step = frame_moove'''
             frame_in = np.concatenate((frame_in[frame_move:], input[loop_i:loop_i+frame_move]))
 
-        frame_out = np.concatenate((frame_out[frame_move:], np.zeros((frame_move,1))))
+        frame_out = np.concatenate((frame_out[frame_move:], np.zeros((frame_move,))))
 
         Y = np.fft.fft(frame_in*win)
-        Y = reformat(Y)
 
         Ya2 = np.power(abs(Y[0:N_eff]), 2)  
+
         '''spec estimation using single frame info.'''
-        Sf = Ya2
-        # Sf = np.convolve(win_freq.flatten(), Ya2.flatten())  
-        # Sf = reformat(Sf)
+        Sf = np.convolve(win_freq.flatten(), Ya2.flatten())  
+
         '''frequency smoothing '''
-        # Sf = Sf[f_win_length:N_eff+f_win_length]  
+        Sf = Sf[f_win_length:N_eff+f_win_length]  
 
         '''initialization'''
         if (loop_i==0):         
@@ -104,6 +164,7 @@ def omlsa(input,fs,plot = None):
             lambda_dav = expected noise spec
             lambda_d = modified expected noise spec
             '''
+            
             lambda_dav = lambda_d = Ya2  
             
             ''' instant SNR estimation'''
@@ -123,6 +184,7 @@ def omlsa(input,fs,plot = None):
             
             eta_2term = np.power(GH1,2) * gamma
 
+        
 
         '''instant SNR'''  
         gamma = np.divide(Ya2 ,np.maximum(lambda_d, 1e-10))
@@ -146,46 +208,29 @@ def omlsa(input,fs,plot = None):
             Smin = np.minimum(Smin,S)
             Smin_sw = np.minimum(Smin_sw, S)
 
-        gama_min = np.divide((Ya2 / Bmin),Smin)
+        gamma_min = np.divide((Ya2 / Bmin),Smin)
         zeta = np.divide(S/Bmin,Smin)
 
         I_f = [0]*N_eff
         
-        
-        # about 0.2 second
-        for i in range(0,N_eff):
-            if(gama_min[i] <gama0 and zeta[i] < zeta0):
-                I_f[i] = 1
-            else:
-                I_f[i] = 0
-                
-        
+        I_f = fast_loop5(N_eff,I_f,gamma0,zeta,zeta0,gamma_min)
 
-        # conv_I = I_f
         conv_I = np.convolve(win_freq, I_f)
-        conv_I = reformat(conv_I)
+        
         '''smooth'''
         conv_I = conv_I[f_win_length:N_eff+f_win_length]
-
+        
 
         Sft = St
-        # idx = find_nonzero(conv_I)
-        I_f = reformat(np.array(I_f))
         
-        '''eq. 26'''
-
-            
+        '''eq. 26'''       
         conv_Y = np.convolve(win_freq.flatten(), (I_f*Ya2).flatten())
-        conv_Y = reformat(conv_Y) 
+
         '''eq. 26'''
         conv_Y = conv_Y[f_win_length:N_eff+f_win_length]
 
-        # about 0.4s
-        for i in range(0,N_eff):
-            if conv_I[i] != 0:
-                Sft[i] = np.divide(conv_Y[i],conv_I[i])
-        
-        
+        Sft = fast_loop1(N_eff,conv_Y,conv_I,Sft)
+             
         St=alpha_s*St+(1-alpha_s)*Sft
         '''updated smoothed spec eq. 27'''
         
@@ -198,64 +243,52 @@ def omlsa(input,fs,plot = None):
         
         gamma_mint = np.divide(Ya2/Bmin, Smint)
         zetat = np.divide(S/Bmin, Smint)
-        qhat = np.ones((N_eff, 1))
+        qhat = np.ones((N_eff, ))
         '''eq. 29 speech absence probability'''
-        phat = np.zeros((N_eff, 1))  
+        phat = np.zeros((N_eff, ))  
         '''eq. 29 init p(speech active|gama)'''
         
         temp = [0]*N_eff
 
+        qhat = fast_loop2(N_eff,gamma_mint,gamma1,zeta0,zetat,qhat)
         
-        # about 0.3s
-        for i in range(0,N_eff):
-            if (gamma_mint[i]>1 and gamma_mint[i]<gama1 and zetat[i]<zeta0):
-                qhat[i] = (gama1-gamma_mint[i]) / (gama1-1)
-            elif gamma_mint[i] >= gama1 or zetat[i] >= zeta0:
-                qhat[i] = 0
-
         phat = np.divide(1,(1+np.divide(qhat,(1-qhat))*(1+eta) * np.exp(-v)))
 
-        # about 0.2s
-        for i in range(0,N_eff):  
-            if (gamma_mint[i] >=gama1 or zetat[i] >=zeta0):
-                phat[i] = 1
-        
+        phat = fast_loop4(N_eff,gamma_mint,gamma1,zetat,zeta0,phat)
+
         alpha_dt = alpha_d + (1-alpha_d) * phat
-
         lambda_dav = alpha_dt * lambda_dav + (1-alpha_dt) * Ya2
-
         lambda_d = lambda_dav * beta
-
+        
 
         if l_mod_lswitch==Vwin:
             '''reinitiate every Vwin frames'''
             l_mod_lswitch=0
             if loop_i == Vwin * frame_move + frame_overlap:
-                SW= np.tile(S,(1,Nwin))
-                SWt= np.tile(St,(1,Nwin))
+                SW= np.tile(S,(Nwin))
+                SWt= np.tile(St,(Nwin))
             else:
-                SW=np.concatenate((SW[:,1:Nwin],Smin_sw),axis = 1)  
-                Smin=np.amin(SW,axis=1); 
-                Smin = reformat(Smin) 
+                SW=np.concatenate((SW[1:Nwin],Smin_sw))  
+                Smin=np.amin(SW); 
                 Smin_sw=S;    
-                SWt=np.concatenate((SWt[:,1:Nwin],Smint_sw),axis = 1)
-                Smint=np.amin(SWt,axis=1);  
-                Smint = reformat(Smint)
+                SWt=np.concatenate((SWt[1:Nwin],Smint_sw))
+                Smint=np.amin(SWt);  
                 Smint_sw=St;   
 
         l_mod_lswitch = l_mod_lswitch + 1
 
+        
+
+        
         gamma = np.divide(Ya2 , np.maximum(lambda_d, 1e-10)) 
         '''update instant SNR'''
         
+
+
         eta = alpha_eta * eta_2term + (1-alpha_eta) * np.maximum(gamma-1, 0)
         '''update smoothed SNR, eq. 32 where eta_2term = GH1 .^ 2 .* gamma '''
 
-        # about 0.1s
-        for i in range(0,N_eff):
-            if eta[i] < eta_min:
-                eta[i] = eta_min
-        
+        eta = fast_loop3(N_eff,eta,eta_min)
 
         v = np.divide(gamma * eta , (1+eta))
 
@@ -266,7 +299,7 @@ def omlsa(input,fs,plot = None):
         eta_2term = np.power(GH1 , 2) * gamma  
         '''eq. 18'''
 
-        X = np.concatenate((np.zeros((3,1)), (G[3:N_eff-1])*(Y[3:N_eff-1]),[[0]]))
+        X = np.concatenate((np.zeros((3,)), (G[3:N_eff-1])*(Y[3:N_eff-1]),[0]))
 
         X_2 = X[1:N_eff-1]
         X_2 = X_2[::-1]
@@ -274,14 +307,12 @@ def omlsa(input,fs,plot = None):
         X = np.concatenate((X,X_other_half))
 
         '''extend the anti-symmetric range of the spectum'''
-        X = X.reshape(len(X),)
         temp = np.real(np.fft.ifft(X))
 
         frame_result = np.power(Cwin,2) * win * temp
-        frame_result = frame_result.reshape(len(frame_result),1)
 
         frame_out = frame_out + frame_result
-
+        
         if(loop_i==0):
             y_out_time[loop_i:loop_i+frame_move] = frame_out[0:frame_move]
             loop_i = loop_i + frame_length
@@ -296,9 +327,8 @@ def omlsa(input,fs,plot = None):
     if plot == "f":
         NFFT = 256
         fig, axes = plt.subplots(nrows=2, ncols=1)
-        Pxx, freqs, bins, im = axes[0].specgram(input,NFFT=NFFT, Fs=fs, noverlap = NFFT/2, vmin= -80)
-        y_out_time_reshape  = y_out_time.reshape(len(y_out_time),)
-        Pxx, freqs, bins, im = axes[1].specgram(y_out_time_reshape,NFFT=NFFT, Fs=fs, noverlap= NFFT/2,vmin= -80)
+        Pxx, freqs, bins, im = axes[0].specgram(raw_input,NFFT=NFFT, Fs=fs, noverlap = NFFT/2)
+        Pxx, freqs, bins, im = axes[1].specgram(y_out_time,NFFT=NFFT, Fs=fs, noverlap= NFFT/2)
         fig.subplots_adjust(right=0.8)
         cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
         fig.colorbar(im, cax=cbar_ax)
